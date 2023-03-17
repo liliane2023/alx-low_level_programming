7eda03019d59062443adbeafd973ecc4e643a211
liliane2023 <lilianekaremera@gmail.com>
2023-03-17 03:49:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A programm that prints a line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle ln");
+return (0);
+}
